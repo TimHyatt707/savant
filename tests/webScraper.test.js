c9b35081f41bfa11f589a8bdb5d676e9041e6c87
TimHyatt707 <timhyatt707@gmail.com>
2018-01-13 15:42:09 -0800

    added jet suite

---
+++ tests/webScraper.test.js
@@ -0,0 +1,6 @@
+const webScraper =  require('../scrapers/webScraper');
+
+test('the function returns an array', () => {
+  let results = webScraper('bogus url');
+  expect(Array.isArray(results)).toBe(true);
+})
