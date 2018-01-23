d4e9ad0a2c8e175230143ff3338bc02d57a3d1fd
TimHyatt707 <timhyatt707@gmail.com>
2018-01-22 19:16:32 -0800

    added complexity to scraper

--- scrapers/webScraper.js
+++ scrapers/webScraper.js
@@ -8,19 +8,51 @@ async function WebScraper(url){
     const doc = await fetch(url);
     const text = await doc.text();
     const $ = cheerio.load(text);
+    // get the domain name to use later for url conversion
+    let domain = url;
+    //remove header
+    if(url.includes('https://')){
+      domain = url.replace('https://', '');
+    }
+    else if(url.includes('http://')){
+      domain = url.replace('http://', '');
+    }
+    //check if url contains any '/'
+    if(domain.includes('/')){
+      let indexOfSlash = domain.indexOf('/');
+      domain = domain.slice(0, indexOfSlash);
+    }
+    console.log(domain);
     $('img').each((i,element) => {
       let link = element.attribs.src;
-      link.startsWith('//') ? link = link.slice(2) : link;
+
+      /* convert relative urls to absolute urls */
+
+      while(link[0].search(/([A-Za-z])/) === -1){
+        link = link.slice(1);
+      }
+
+      // check for static images and append domain if necessary
+
+      //add header
       !link.startsWith('http') ? link = 'https://' + link : link;
+
+      console.log(link);
+
       imageUrls.push(link);
     });
     imageUrls.forEach( async imageUrl => {
+      try {
       let image = await fetch(imageUrl);
       results.push(image);
+      }
+      catch (error) {
+        throw new Error(error.message)
+      }
     });
     return results;
 } catch (error) {
-    console.log(error);
+    throw new Error('Bad img url', error)
   }
 }
 
